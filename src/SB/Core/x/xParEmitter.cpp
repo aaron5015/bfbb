dabcf7e582cc688ac39a8225606f24068a132f0b
Joey Ballentine <joeyjballentine@gmail.com>
2026-09-15 08:35:16 -0500

    fix(particles): Clear all of the emit event's custom settings
    0x16C covers xParEmitterCustomSettings at 32 bits. At 64 it is 368 bytes,
    and the high half of emit_volume kept whatever was on the stack.

--- src/SB/Core/x/xParEmitter.cpp
+++ src/SB/Core/x/xParEmitter.cpp
@@ -226,7 +226,7 @@ S32 xParEmitterEventCB(xBase* to, xBase* from, U32 toEvent, const F32* toParam,
         }
         break;
     case eEventEmit:
-        memset(&sp8, 0, 0x16C);
+        memset(&sp8, 0, sizeof(sp8));
         // A thirtieth of a second's worth: two console frames.
 #ifdef PLATFORM_PC
         xParEmitterEmitCustom((xParEmitter*)from, 0.033333335f, &sp8, 2.0f);
