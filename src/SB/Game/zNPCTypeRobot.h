11964bbb63a6b40c98cd4c9fd0d6e4a7a74bde38
aaron5015 <sabhaxer@gmail.com>
2026-09-19 07:25:21 -0500

    Expose Sleepy detection helpers for Buddy

--- src/SB/Game/zNPCTypeRobot.h
+++ src/SB/Game/zNPCTypeRobot.h
@@ -697,6 +697,8 @@ void ROBO_KillEffects();
 void zNPCFodBzzt_ResetDanceParty();
 void ROBO_InitEffects();
 void zNPCRobot_Timestep(xScene* xscn, F32 dt);
+S32 zNPCSleepy_IsAsleep(zNPCCommon* npc);
+S32 zNPCSleepy_IsInDetectionRange(zNPCCommon* npc, const xVec3* pos);
 xAnimTable* ZNPC_AnimTable_ArfArf();
 xAnimTable* ZNPC_AnimTable_ArfDog();
 xAnimTable* ZNPC_AnimTable_Chuck();
