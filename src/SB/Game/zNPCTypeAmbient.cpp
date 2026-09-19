ea30f05228b8b0f0bedc70bf82cff09df606ac4f
aaron5015 <sabhaxer@gmail.com>
2026-09-19 04:47:41 -0500

    buddy: let jellyfish detect closer buddy

--- src/SB/Game/zNPCTypeAmbient.cpp
+++ src/SB/Game/zNPCTypeAmbient.cpp
@@ -725,6 +725,19 @@ S32 JELY_grul_getAngry(xGoal* rawgoal, void* p1, en_trantype* trantype, F32 f, v
 
     dst_sq = npc->XYZDstSqToPlayer(NULL);
 
+    if (zBuddy_IsAvailable())
+    {
+        const xVec3* buddy_pos = zBuddy_GetTargetPosition();
+        if (buddy_pos != NULL)
+        {
+            F32 buddy_dst_sq = npc->XZDstSqToPos(buddy_pos, 0, 0);
+            if (buddy_dst_sq < dst_sq)
+            {
+                dst_sq = buddy_dst_sq;
+            }
+        }
+    }
+
     if (dst_sq < SQ(5.0f))
     {
         npc->cnt_angerLevel++;
